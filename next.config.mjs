822c87b24ff86c78b2826c2ec89d28a522e5819d
Whirosee41-Droid <whirose41@gmail.com>
2026-02-25 18:03:10 +0800

    fix: static export for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  output: "export",
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
